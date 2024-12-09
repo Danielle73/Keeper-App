ff43621d099382529841e08fd3dba76fcaf72854
Danielle <daniellelee73@gmail.com>
2024-12-09 23:09:59 +0000

    feat: changes made to apply the paragraph styles to the notes component

--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -1,9 +1,9 @@
 const Note = () => {
     return ( 
         <>
-        <div>
-            <h1>This is the note title</h1>
-            <p>This is the note content</p>
+        <div className="note">
+            <h1 className="note_h1">This is the note title</h1>
+            <p className="note_p">This is the note content</p>
         </div>
         </>
      );
